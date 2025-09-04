5da658e5cb1ffcd6348bbd282a2bc8d4bd1dc363
Aaron2065 <20001286@alumnos.utleon.edu.mx>
2025-09-04 12:29:07 -0600

    Creacion de BllProducto para el manejo de las reglas de negocio para los metodos obtener, agregar, modificar y eliminar producto

--- CapaNegocio/BllProducto.cs
+++ CapaNegocio/BllProducto.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿using CapaDatos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VO;
 
 namespace CapaNegocio
 {
-    internal class BllProducto
+    public class BllProducto
     {
+        public static List<ProductoVO> GetListaProductos()
+        {
+            try
+            {
+                return DalProducto.GetListaProductos();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener la lista de productos: " + ex.Message);
+            }
+        }
+
+        // Obtener producto por ID
+        public static ProductoVO GetProductoByID(int productoID)
+        {
+            try
+            {
+                return DalProducto.GetProductoByID(productoID);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener el producto: " + ex.Message);
+            }
+        }
+
+        // Insertar producto
+        public static void InsProducto(int categoriaID, string nombre, string descripcion, decimal precio, string urlFoto, bool disponible)
+        {
+            try
+            {
+                DalProducto.InsertarProducto(categoriaID, nombre, descripcion, precio, urlFoto, disponible);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al insertar el producto: " + ex.Message);
+            }
+        }
+
+        // Actualizar producto
+        public static void UpdProducto(int productoID, int categoriaID, string nombre, string descripcion, decimal precio, string urlFoto, bool disponible)
+        {
+            try
+            {
+                DalProducto.ActualizarProducto(productoID, categoriaID, nombre, descripcion, precio, urlFoto, disponible);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al actualizar el producto: " + ex.Message);
+            }
+        }
+
+        // Eliminar producto
+        public static void DelProducto(int productoID)
+        {
+            try
+            {
+                DalProducto.EliminarProducto(productoID);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al eliminar el producto: " + ex.Message);
+            }
+        }
     }
 }
