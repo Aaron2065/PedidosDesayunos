060bd99a8f1da1ddf12a5869bcd225655a9d8073
Aaron2065 <20001286@alumnos.utleon.edu.mx>
2025-09-05 00:57:36 -0600

    Creacion de DalPedido para el acceso a los datos mediante triggers de listar, actualizar, modificar y eliminar pedido

--- CapaDatos/DalPedido.cs
+++ CapaDatos/DalPedido.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VO;
 
 namespace CapaDatos
 {
-    internal class DalPedido
+    public class DalPedido
     {
+        // Listar pedidos
+        public static List<PedidoVO> GetListaPedidos()
+        {
+            DataSet ds = MetodoDatos.ExecuteDataSet("Pedidos_Listar");
+            List<PedidoVO> lista = new List<PedidoVO>();
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                lista.Add(new PedidoVO(dr));
+            }
+
+            return lista;
+        }
+
+        // Insertar pedido (solo cliente y notas, porque así está el SP)
+        public static int InsertarPedido(int clienteID, string notas)
+        {
+            DataSet ds = MetodoDatos.ExecuteDataSet("Pedidos_Insertar",
+                "@ClienteID", clienteID,
+                "@Notas", notas);
+
+            return Convert.ToInt32(ds.Tables[0].Rows[0]["NuevoPedidoID"]);
+        }
+
+        // Actualizar pedido
+        public static int ActualizarPedido(int pedidoID, int clienteID, bool estado, string notas)
+        {
+            return MetodoDatos.ExecuteNonQuey("Pedidos_Actualizar",
+                "@PedidoID", pedidoID,
+                "@ClienteID", clienteID,
+                "@Estado", estado,
+                "@Notas", notas);
+        }
+
+        // Eliminar pedido
+        public static int EliminarPedido(int pedidoID)
+        {
+            return MetodoDatos.ExecuteNonQuey("Pedidos_Eliminar",
+                "@PedidoID", pedidoID);
+        }
+
+        // Obtener pedido por ID
+        public static PedidoVO GetPedidoByID(int pedidoID)
+        {
+            DataSet ds = MetodoDatos.ExecuteDataSet("Pedidos_GetByID",
+                "@PedidoID", pedidoID);
+
+            if (ds.Tables[0].Rows.Count == 0) return null;
+
+            return new PedidoVO(ds.Tables[0].Rows[0]);
+        }
     }
 }
