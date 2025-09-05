1872f2e3d983ad1d18cb0e884f9ea4d328e2530a
Aaron2065 <20001286@alumnos.utleon.edu.mx>
2025-09-05 00:42:16 -0600

    Creación de PedidoVO para la definición de atributos, propiedades y constructores

---
+++ VO/PedidoVO.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO
+{
+    public class PedidoVO
+    {
+
+        private int _pedidoID;
+        private int _clienteID;
+        private string _clienteNombre;
+        private DateTime _fechaPedido;
+        private decimal _total;
+        private bool _estado;
+        private string _notas;
+
+        public int PedidoID 
+        { 
+            get => _pedidoID; 
+            set => _pedidoID = value; 
+        }
+        public int ClienteID 
+        { 
+            get => _clienteID; 
+            set => _clienteID = value; 
+        }
+        public string ClienteNombre 
+        { 
+            get => _clienteNombre; 
+            set => _clienteNombre = value; 
+        }
+        public DateTime FechaPedido 
+        { 
+            get => _fechaPedido; 
+            set => _fechaPedido = value; 
+        }
+        public decimal Total 
+        { 
+            get => _total; 
+            set => _total = value; 
+        }
+        public bool Estado 
+        { 
+            get => _estado; 
+            set => _estado = value; 
+        }
+        public string Notas 
+        { 
+            get => _notas; 
+            set => _notas = value; 
+        }
+
+        public PedidoVO()
+        {
+            PedidoID = 0;
+            ClienteID = 0;
+            ClienteNombre = string.Empty;
+            FechaPedido = DateTime.Now;
+            Total = 0;
+            Estado = true;
+            Notas = string.Empty;
+        }
+
+        public PedidoVO(DataRow dr)
+        {
+            PedidoID = Convert.ToInt32(dr["PedidoID"]);
+            ClienteID = Convert.ToInt32(dr["ClienteID"]);
+            ClienteNombre = dr.Table.Columns.Contains("Cliente") ? dr["Cliente"].ToString() : string.Empty;
+            FechaPedido = Convert.ToDateTime(dr["FechaPedido"]);
+            Total = Convert.ToDecimal(dr["Total"]);
+            Estado = Convert.ToBoolean(dr["Estado"]);
+            Notas = dr["Notas"].ToString();
+        }
+    }
+}
