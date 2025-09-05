a36fc6aeb206c818ee28b7c7a1b38c9658c4aa37
Aaron2065 <20001286@alumnos.utleon.edu.mx>
2025-09-05 00:55:48 -0600

    Creación de BllDetallePedido para el manejo de las reglas de negocio para los métodos obtener, agregar, modificar y eliminar detallePedido.

---
+++ CapaNegocio/BllDetallePedido.cs
@@ -0,0 +1,28 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace CapaNegocio
+{
+    public class BllDetallePedido
+    {
+        public static List<DetallePedidoVO> GetDetallesByPedido(int pedidoID)
+        {
+            return DalDetallePedido.GetDetallesByPedido(pedidoID);
+        }
+
+        public static void InsertDetalle(int pedidoID, int productoID, int cantidad, decimal precioUnitario)
+        {
+            DalDetallePedido.InsertarDetalle(pedidoID, productoID, cantidad, precioUnitario);
+        }
+
+        public static void DeleteDetalle(int detalleID)
+        {
+            DalDetallePedido.EliminarDetalle(detalleID);
+        }
+    }
+}
