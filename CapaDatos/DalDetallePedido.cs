e6bac54dd7a80e51da1f66b653bda807b839b937
Aaron2065 <20001286@alumnos.utleon.edu.mx>
2025-09-05 00:58:09 -0600

    Creacion de DalDetallePedido para el acceso a los datos mediante triggers de listar, actualizar, modificar y eliminar detallePedido

---
+++ CapaDatos/DalDetallePedido.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace CapaDatos
+{
+    public class DalDetallePedido
+    {
+        // Listar detalles de un pedido
+        public static List<DetallePedidoVO> GetDetallesByPedido(int pedidoID)
+        {
+            DataSet ds = MetodoDatos.ExecuteDataSet("DetallePedido_ListarPorPedido",
+                "@PedidoID", pedidoID);
+
+            List<DetallePedidoVO> lista = new List<DetallePedidoVO>();
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                lista.Add(new DetallePedidoVO(dr));
+            }
+
+            return lista;
+        }
+
+        // Insertar detalle
+        public static int InsertarDetalle(int pedidoID, int productoID, int cantidad, decimal precioUnitario)
+        {
+            return MetodoDatos.ExecuteNonQuey("DetallePedido_Insertar",
+                "@PedidoID", pedidoID,
+                "@ProductoID", productoID,
+                "@Cantidad", cantidad,
+                "@PrecioUnitario", precioUnitario);
+        }
+
+        // Eliminar detalle
+        public static int EliminarDetalle(int detalleID)
+        {
+            return MetodoDatos.ExecuteNonQuey("DetallePedido_Eliminar",
+                "@DetalleID", detalleID);
+        }
+    }
+}
